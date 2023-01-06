bc50293510f5c75348eb9cd0f482d5f84d263064
Daniel Schneider <daniel.schneider@agrotis.com>
2023-01-06 17:04:41 -0300

    fix: remove late variable initialization

--- lib/polybool.dart
+++ lib/polybool.dart
@@ -8,7 +8,7 @@ import 'types.dart';
 typedef Selector = SegmentList Function(CombinedSegmentLists);
 
 class PolyBool {
-  late BuildLog log;
+  BuildLog? log;
 
   SegmentList segments(RegionPolygon poly) {
     var i = Intersecter(true);
@@ -33,7 +33,7 @@ class PolyBool {
   }
 
   SegmentList selectUnion(CombinedSegmentLists combined) {
-    var result = SegmentSelector.union(combined.combined, log);
+    var result = SegmentSelector.union(combined.combined, log!);
     result.inverted = combined.inverted1 || combined.inverted2;
 
     return result;
@@ -50,21 +50,21 @@ class PolyBool {
   }
 
   SegmentList selectDifference(CombinedSegmentLists combined) {
-    var result = SegmentSelector.difference(combined.combined, log);
+    var result = SegmentSelector.difference(combined.combined, log!);
     result.inverted = combined.inverted1 && !combined.inverted2;
 
     return result;
   }
 
   SegmentList selectDifferenceRev(CombinedSegmentLists combined) {
-    var result = SegmentSelector.differenceRev(combined.combined, log);
+    var result = SegmentSelector.differenceRev(combined.combined, log!);
     result.inverted = !combined.inverted1 && combined.inverted2;
 
     return result;
   }
 
   SegmentList selectXor(CombinedSegmentLists combined) {
-    var result = SegmentSelector.xor(combined.combined, log);
+    var result = SegmentSelector.xor(combined.combined, log!);
     result.inverted = combined.inverted1 != combined.inverted2;
 
     return result;
